6d2b559a6922180f6f4b33dca9501c8eaf746c8b
vpnineh <fffarbod@bk.ru>
2025-07-09 18:03:41 +0330

    Update config.php

--- config.php
+++ config.php
@@ -6,6 +6,6 @@ $subscription_urls = [
     "https://github.com/Aa64n/Aa64n-/raw/refs/heads/main/family",
     "https://github.com/Aa64n/Aa64n-/raw/refs/heads/main/freind",
     "https://github.com/Aa64n/Aa64n-/raw/refs/heads/main/wir",
-    "https://raw.githubusercontent.com/Pawdroid/Free-servers/refs/heads/main/sub"
+    "https://raw.githubusercontent.com/Pawdroid/Free-servers/refs/heads/main/sub",
     "https://drive.google.com/uc?export=download&id=1-EopH8hKLwaRJ3kxm3-40x4CZQ3prAzP"
   ];
