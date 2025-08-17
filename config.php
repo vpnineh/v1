63ce58153173fef196beecb36b5ff68e0a55ba27
vpnineh <fffarbod@bk.ru>
2025-08-17 23:53:06 +0330

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,12 @@
 <?php
 
 $subscription_urls = [
-    "https://github.com/Aa64n/Aa64n-/raw/refs/heads/main/b",
-    "https://github.com/Aa64n/Aa64n-/raw/refs/heads/main/backk%20up%20new",
-    "https://github.com/Aa64n/Aa64n-/raw/refs/heads/main/family",
-    "https://github.com/Aa64n/Aa64n-/raw/refs/heads/main/freind",
-    "https://github.com/Aa64n/Aa64n-/raw/refs/heads/main/wir",
-    "https://raw.githubusercontent.com/Pawdroid/Free-servers/refs/heads/main/sub",
-    "https://drive.google.com/uc?export=download&id=1-EopH8hKLwaRJ3kxm3-40x4CZQ3prAzP"
+    #"https://github.com/Aa64n/Aa64n-/raw/refs/heads/main/b",
+    #"https://github.com/Aa64n/Aa64n-/raw/refs/heads/main/backk%20up%20new",
+    #"https://github.com/Aa64n/Aa64n-/raw/refs/heads/main/family",
+   # "https://github.com/Aa64n/Aa64n-/raw/refs/heads/main/freind",
+    #"https://github.com/Aa64n/Aa64n-/raw/refs/heads/main/wir",
+    #"https://raw.githubusercontent.com/Pawdroid/Free-servers/refs/heads/main/sub",
+    "https://raw.githubusercontent.com/hamedp-71/Trojan/refs/heads/main/hp.txt",
+    #"https://drive.google.com/uc?export=download&id=1-EopH8hKLwaRJ3kxm3-40x4CZQ3prAzP"
   ];
